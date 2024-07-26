ecdd72f97b94744778d46a3134e49a3a534e96a3
MR~TEKNO_~VRS <aakubiasaa334@gmail.com>
2024-07-26 15:10:39 +0700

    Add files via upload

---
+++ tampilan-termux.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# tampilan by MR~TEKNO_~VRS
+# start
+#tampilan
+read -p "what your name : " nama
+   echo "hello $nama" | lolcat
+  sleep 3
+ clear
+ sleep 4
+ toilet -f slant --gay "$nama" | lolcat
+
+# login termux
+echo " ========================================" | lolcat
+echo "             welcom to termux" | lolcat
+echo " ========================================" | lolcat
+
+echo
+echo
